2b904f3fc581c8cb4cbb2d2c77286f055d52db9b
KJ Wilson <yourboykjdev@gmail.com>
2020-06-29 14:53:45 -0700

    first commit 🚀

---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class About extends Component {
+  render() {
+    return (
+      // const { title, date, picture, excerpt, link }
+      <div className="about"></div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./styles.css";
+
+class Home extends Component {
+  render() {
+    return (
+      // const { title, date, picture, excerpt, link }
+      <div className="home"></div>
+    );
+  }
+}
+
+export default Home;
