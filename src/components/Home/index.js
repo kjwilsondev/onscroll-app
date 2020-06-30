031a9ad4989de2269c05ed36a1645afcbd2412b8
KJ Wilson <yourboykjdev@gmail.com>
2020-06-29 17:35:35 -0700

    added image and bottom right text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./styles.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 class Home extends Component {
   render() {
     return (
       // const { title, date, picture, excerpt, link }
-      <div className="home"></div>
+      <div className="title">
+        <p>DJ Project</p>
+      </div>
     );
   }
 }
