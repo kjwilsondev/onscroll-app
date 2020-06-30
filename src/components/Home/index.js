3570f7aeb961a65fc60676f63b16c01181696688
KJ Wilson <yourboykjdev@gmail.com>
2020-06-29 21:40:02 -0700

    finalized pic and text

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,11 +4,20 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    AOS.init({
+      duration: 1200,
+    });
+  }
   render() {
     return (
       // const { title, date, picture, excerpt, link }
       <div className="title">
-        <p>DJ Project</p>
+        <div className="title-text" data-aos="fade-up">
+          <p>DJPROJECT</p>
+          {/* <p className="sub-text">TRANSFORM YOUR COMMUNITY</p> */}
+        </div>
       </div>
     );
   }
